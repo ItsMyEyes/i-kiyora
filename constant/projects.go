99874607035317735a66ef61b12900bc655ed1a5
And <andiselalu782@gmail.com>
2023-08-29 14:12:56 +0700

    update

--- constant/projects.go
+++ constant/projects.go
@@ -8,7 +8,7 @@ import (
 var (
 	Answer                     string
 	Kiyora                     = "Kiyora"
-	Version                    = "v0.2.11"
+	Version                    = "v0.2.14"
 	MinimalVersionGolang       = "1.19.0"
 	ProjectLink                = "https://github.com/ItsMyEyes/kiyora_v2.git"
 	Replace                    = "github.com/ItsMyEyes/kiyora_v2"
